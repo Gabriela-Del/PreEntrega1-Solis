393869b9439f193cb9933a5e13d3838259642506
Gabriela-Del <67982100+Gabriela-Del@users.noreply.github.com>
2022-12-01 22:03:50 -0300

    barra navbar

---
+++ src/components/CartWidget.js
@@ -0,0 +1,17 @@
+import React, {useState} from 'react';
+
+const CartWidget = () => {
+
+    const [cont, setCont] = useState(0)
+
+    const sumar = () => setCont (cont+1)
+  return (
+    <button onClick={sumar} type="button" className="btn btn-success mx-2">
+        <i className="bi bi-cart-check"></i>
+        <p>{cont}</p> 
+    </button>
+    
+  )
+}
+
+export default CartWidget
